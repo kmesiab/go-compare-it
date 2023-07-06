23b79e6bec28d88dc0586a0ca15616af8508ed60
Kevin Mesiab <kmesiab@gmail.com>
2023-07-05 21:30:26 -0700

    Add basic logger

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,1 +1,25 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		c.
+			// before request
+			c.Next()
+		// after request
+
+		latency := time.Since(t)
+		log.Printf("latency: %d", latency)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
